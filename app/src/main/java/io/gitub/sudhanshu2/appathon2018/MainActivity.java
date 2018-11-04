eb3d7088c2047b8ecd3c60a4585187ac376949db
Shlok Natarajan <snatarajan542@rsdmo.org>
2018-11-04 07:37:29 -0500

    Buttons if statements

--- app/src/main/java/io/gitub/sudhanshu2/appathon2018/MainActivity.java
+++ app/src/main/java/io/gitub/sudhanshu2/appathon2018/MainActivity.java
@@ -25,11 +25,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         final EditText editText = (EditText) findViewById(R.id.text_input);
         editText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                 do stuff when button is pressed
+//              Clears the textbox when user clicks in
                 editText.getText().clear();
             }
         });
@@ -38,10 +39,36 @@ public class MainActivity extends AppCompatActivity {
         powerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                 do stuff when button is pressed
+//              Activate the application stuff
                 powerButton.setSelected(!powerButton.isSelected());
             }
         });
+
+        final Switch startAux = (Switch) findViewById(R.id.start_with_aux);
+        final Switch stopMusic = (Switch) findViewById(R.id.stop_music);
+        final Switch lowerVolume = (Switch) findViewById(R.id.lower_volume);
+        Switch playTone = (Switch) findViewById(R.id.play_tone);
+        final Button submitButton = findViewById(R.id.submit_button);
+        final String wordToLookFor;
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              This updates the settings of the app
+                if(startAux.isChecked()) {
+                    // put code here for starting app on aux connection
+                }
+                if (stopMusic.isChecked()) {
+                    // put code here that stops music when word heard
+                }
+                if (lowerVolume.isChecked()) {
+                    // put code here that lowers volume when word heard
+                }
+                wordToLookFor = editText.getText().toString();
+            }
+        });
+
+
+
 
 
     }
