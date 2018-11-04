b826684b1203c6b4a53029fdb0c57f27f6378f21
Sudhanshu Agarwal <sudhanshuagarwal2002@gmail.com>
2018-11-03 21:22:23 -0400

    Initial App

---
+++ app/src/main/java/io/gitub/sudhanshu2/appathon2018/MainActivity.java
@@ -0,0 +1,13 @@
+package io.gitub.sudhanshu2.appathon2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
