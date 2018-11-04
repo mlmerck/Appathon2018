640daf9023948c47934913b10f8db0746a14f0c0
Shlok Natarajan <snatarajan542@rsdmo.org>
2018-11-04 05:49:16 -0500

    Initial
    Initial

--- app/src/main/java/io/gitub/sudhanshu2/appathon2018/MainActivity.java
+++ app/src/main/java/io/gitub/sudhanshu2/appathon2018/MainActivity.java
@@ -1,7 +1,13 @@
 package io.gitub.sudhanshu2.appathon2018;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import edu.cmu.pocketsphinx.Assets;
 import edu.cmu.pocketsphinx.Hypothesis;
@@ -12,9 +18,31 @@ import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.text_input);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                 do stuff when button is pressed
+                editText.getText().clear();
+            }
+        });
+
+        final Button powerButton = findViewById(R.id.power_button);
+        powerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                 do stuff when button is pressed
+                powerButton.setSelected(!powerButton.isSelected());
+            }
+        });
+
+
     }
 }
