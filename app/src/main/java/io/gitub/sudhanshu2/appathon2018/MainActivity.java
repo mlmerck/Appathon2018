977706f3e17ff3a519605bb319effd0ea70f4f5c
Sudhanshu Agarwal <sudhanshuagarwal2002@gmail.com>
2018-11-03 23:22:14 -0400

    Added PocketSphinx

--- app/src/main/java/io/gitub/sudhanshu2/appathon2018/MainActivity.java
+++ app/src/main/java/io/gitub/sudhanshu2/appathon2018/MainActivity.java
@@ -3,6 +3,13 @@ package io.gitub.sudhanshu2.appathon2018;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
